e73267b3821582954aa6736aeefcd17faf3f34b8
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2022-07-02 13:09:01 -0400

    Moved data server example in electron backend to a worker thread.

--- electron/electronstart/index.js
+++ electron/electronstart/index.js
@@ -1,19 +1,10 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+//@ts-ignore
+const { Worker } = require("worker_threads");
 const { app, BrowserWindow, nativeImage } = require("electron");
 const ipcMain = require("electron").ipcMain;
-const { promises: fs } = require("fs");
 const path = require("path");
-const ws = require("ws");
 const { session } = require("electron");
-const zlib = require("zlib");
-const compress = (input) => {
-    return zlib.deflateSync(input);
-};
-const deCompress = (input) => {
-    //@ts-ignore
-    return zlib.inflateSync(input);
-};
 /*
 *fix webgl context lost
 https://github.com/electron/electron/issues/11934
@@ -33,7 +24,7 @@ const APP_INIT = async () => {
         });
     });
     const editorWindow = await CreateMainWindow();
-    setUpDataServer();
+    const worker = new Worker("./electronstart/server/index.js");
 };
 app.whenReady().then(async () => {
     await APP_INIT();
@@ -70,74 +61,4 @@ const CreateMainWindow = async () => {
         }, 1000);
     });
     return mainWindow;
-};
-const setUpDataServer = () => {
-    const dataMessage = {
-        0: async (data, ws) => {
-            let regionX = data[5];
-            if (data[4] == 1) {
-                regionX = regionX * -1;
-            }
-            let regionY = data[7];
-            if (data[6] == 1) {
-                regionY = regionY * -1;
-            }
-            let regionZ = data[9];
-            if (data[8] == 1) {
-                regionZ = regionZ * -1;
-            }
-            const name = `region_${regionX}_${regionY}_${regionZ}.dved`;
-            const compressed = compress(data.buffer);
-            await fs.writeFile(`./data/${name}`, compressed);
-        },
-        1: async (data, ws) => {
-            let regionX = data[2];
-            if (data[1] == 1) {
-                regionX = regionX * -1;
-            }
-            let regionY = data[4];
-            if (data[3] == 1) {
-                regionY = regionY * -1;
-            }
-            let regionZ = data[6];
-            if (data[5] == 1) {
-                regionZ = regionZ * -1;
-            }
-            const name = `region_${regionX}_${regionY}_${regionZ}.dved`;
-            const directory = await fs.readdir("./data");
-            for (const file of directory) {
-                if (file.includes(name)) {
-                    const path = `./data/${file}`;
-                    const rawData = await fs.readFile(path);
-                    const data = deCompress(rawData);
-                    const _8Array = new Uint8Array(data);
-                    let dataview = new DataView(_8Array.buffer);
-                    const returnArray = new Uint32Array(_8Array.length / 4);
-                    let k = 0;
-                    for (let i = 0; i < _8Array.length; i += 4) {
-                        returnArray[k] = dataview.getUint32(i, true);
-                        k++;
-                    }
-                    ws.send(returnArray);
-                }
-            }
-        },
-    };
-    const wss = new ws.WebSocketServer({
-        port: 8080,
-        maxPayload: 1280 * 800000,
-    });
-    wss.on("connection", function connection(ws) {
-        console.log("connected");
-        ws.binaryType = "arraybuffer";
-        ws.on("message", async function message(data) {
-            const dataArray = new Uint32Array(data);
-            const message = dataArray[0];
-            console.log("MESSAGE => ", message);
-            if (dataMessage[message]) {
-                dataMessage[message](dataArray, ws);
-            }
-        });
-        //ws.send("something");
-    });
 };
