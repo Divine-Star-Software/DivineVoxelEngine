52f2bca098d51a4b0ae9acae77653233ce1b2b0b
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2024-04-08 20:06:17 -0400

    updated

--- divinevoxel/default/src/Builder/Shapes/default/Box/Box.voxel.shape.ts
+++ divinevoxel/default/src/Builder/Shapes/default/Box/Box.voxel.shape.ts
@@ -4,11 +4,105 @@ import type { DirectionNames } from "@divinevoxel/core/Types/Util.types.js";
 import { OverrideManager } from "../../../Rules/Overrides/OverridesManager.js";
 import { ShapeTool } from "../../ShapeTool.js";
 import { QuadVertexData } from "../../../Classes/VertexData.js";
+import { Vec2Array, Vec3Array } from "@divinevoxel/core/Math/index.js";
+import { VoxelGeometry } from "Builder/Geometry/VoxelGeometry.js";
+import { WorldSpaces } from "@divinevoxel/core/Data/World/WorldSpaces.js";
 
 const animationState = new QuadVertexData();
-
+const Quads: Record<
+  DirectionNames,
+  [
+    points: [Vec3Array, Vec3Array, Vec3Array, Vec3Array],
+    uvs: [Vec2Array, Vec2Array, Vec2Array, Vec2Array]
+  ]
+> = {
+  top: [
+    [
+      [1, 1, 0],
+      [0, 1, 0],
+      [0, 1, 1],
+      [1, 1, 1],
+    ],
+    [
+      [0, 1],
+      [0, 0],
+      [1, 0],
+      [1, 1],
+    ],
+  ],
+  bottom: [
+    [
+      [1, 0, 1],
+      [0, 0, 1],
+      [0, 0, 0],
+      [1, 0, 0],
+    ],
+    [
+      [1, 0],
+      [0, 0],
+      [0, 1],
+      [1, 1],
+    ],
+  ],
+  north: [
+    [
+      [1, 0, 1],
+      [1, 1, 1],
+      [0, 1, 1],
+      [0, 0, 1],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [0, 0],
+    ],
+  ],
+  south: [
+    [
+      [0, 0, 0],
+      [0, 1, 0],
+      [1, 1, 0],
+      [1, 0, 0],
+    ],
+    [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ],
+  ],
+  east: [
+    [
+      [1, 0, 0],
+      [1, 1, 0],
+      [1, 1, 1],
+      [1, 0, 1],
+    ],
+    [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ],
+  ],
+  west: [
+    [
+      [0, 0, 1],
+      [0, 1, 1],
+      [0, 1, 0],
+      [0, 0, 0],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [0, 0],
+    ],
+  ],
+};
 export const BoxVoxelShape = {
- _createFace() {
+  /*  _createFace() {
   animationState.setAll(
     ShapeTool.data.voxel.getSubstance() == "#dve_flora" ? 3 : 0
   )
@@ -21,244 +115,298 @@ export const BoxVoxelShape = {
    .overlayTexture.add(ShapeTool.data.getOverlayTextures())
    .animationState.add(animationState)
    .create();
- },
- add: {
-  top() {
-   ShapeTool.builder.quad.setDirection("top").updatePosition(0.5, 1, 0.5);
-   BoxVoxelShape._createFace();
+ }, */
+  add: {
+    top() {
+      //  ShapeTool.builder.quad.setDirection("top").updatePosition(0.5, 1, 0.5);
+      //  BoxVoxelShape._createFace();
+      const { x, y, z } = ShapeTool.data.voxel;
+
+      const voxelPOS = WorldSpaces.voxel.getPositionXYZ(x, y, z);
+      VoxelGeometry.addQuad(
+        ShapeTool.data,
+        voxelPOS,
+        false,
+        Quads.top[0],
+        Quads.top[1]
+      );
+    },
+    bottom() {
+      const { x, y, z } = ShapeTool.data.voxel;
+
+      const voxelPOS = WorldSpaces.voxel.getPositionXYZ(x, y, z);
+      VoxelGeometry.addQuad(
+        ShapeTool.data,
+        voxelPOS,
+        false,
+        Quads.bottom[0],
+        Quads.bottom[1]
+      );
+      //  BoxVoxelShape._createFace();
+    },
+    north() {
+      const { x, y, z } = ShapeTool.data.voxel;
+
+      const voxelPOS = WorldSpaces.voxel.getPositionXYZ(x, y, z);
+      VoxelGeometry.addQuad(
+        ShapeTool.data,
+        voxelPOS,
+        false,
+        Quads.north[0],
+        Quads.north[1]
+      );
+      // BoxVoxelShape._createFace();
+    },
+    south() {
+      const { x, y, z } = ShapeTool.data.voxel;
+
+      const voxelPOS = WorldSpaces.voxel.getPositionXYZ(x, y, z);
+      VoxelGeometry.addQuad(
+        ShapeTool.data,
+        voxelPOS,
+        false,
+        Quads.south[0],
+        Quads.south[1]
+      );
+      //  BoxVoxelShape._createFace();
+    },
+    east() {
+      const { x, y, z } = ShapeTool.data.voxel;
+
+      const voxelPOS = WorldSpaces.voxel.getPositionXYZ(x, y, z);
+      VoxelGeometry.addQuad(
+        ShapeTool.data,
+        voxelPOS,
+        false,
+        Quads.east[0],
+        Quads.east[1]
+      );
+      //   BoxVoxelShape._createFace();
+    },
+    west() {
+      const { x, y, z } = ShapeTool.data.voxel;
+      const voxelPOS = WorldSpaces.voxel.getPositionXYZ(x, y, z);
+      VoxelGeometry.addQuad(
+        ShapeTool.data,
+        voxelPOS,
+        false,
+        Quads.west[0],
+        Quads.west[1]
+      );
+      //  BoxVoxelShape._createFace();
+    },
   },
-  bottom() {
-   ShapeTool.builder.quad.setDirection("bottom").updatePosition(0.5, 0, 0.5);
-   BoxVoxelShape._createFace();
-  },
-  north() {
-   ShapeTool.builder.quad.setDirection("north").updatePosition(0.5, 0.5, 1);
-   BoxVoxelShape._createFace();
-  },
-  south() {
-   ShapeTool.builder.quad.setDirection("south").updatePosition(0.5, 0.5, 0);
-   BoxVoxelShape._createFace();
-  },
-  east() {
-   ShapeTool.builder.quad.setDirection("east").updatePosition(1, 0.5, 0.5);
-   BoxVoxelShape._createFace();
-  },
-  west() {
-   ShapeTool.builder.quad.setDirection("west").updatePosition(0, 0.5, 0.5);
-   BoxVoxelShape._createFace();
-  },
- },
 };
 
 //cullface
 OverrideManager.registerOverride("CullFace", "#dve_box", "#dve_box", (data) => {
- return BoxCullFunctions[data.face](data);
+  return BoxCullFunctions[data.face](data);
 });
 OverrideManager.registerOverride("CullFace", "#dve_box", "Panel", (data) => {
- return true;
+  return true;
 });
 OverrideManager.registerOverride(
- "DarkenFaceUnderneath",
- "#dve_box",
- "All",
- (data) => {
-  return true;
- }
-);
-OverrideManager.registerOverride(
- "CullFace",
- "#dve_box",
- "#dve_halfbox",
- (data) => {
-  if (data.face == "top") {
-   if (data.neighborVoxel.getShapeState() == 0) {
+  "DarkenFaceUnderneath",
+  "#dve_box",
+  "All",
+  (data) => {
     return true;
-   }
-   return false;
   }
-  return true;
- }
 );
 OverrideManager.registerOverride(
- "CullFace",
- "#dve_box",
- "#dve_stair",
- (data) => {
-  StairCullFunctions[data.face](data);
-  return true;
- }
+  "CullFace",
+  "#dve_box",
+  "#dve_halfbox",
+  (data) => {
+    if (data.face == "top") {
+      if (data.neighborVoxel.getShapeState() == 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+);
+OverrideManager.registerOverride(
+  "CullFace",
+  "#dve_box",
+  "#dve_stair",
+  (data) => {
+    StairCullFunctions[data.face](data);
+    return true;
+  }
 );
 //ao
 OverrideManager.registerOverride("AO", "#dve_box", "Panel", (data) => {
- return false;
+  return false;
 });
 OverrideManager.registerOverride("AO", "#dve_box", "#dve_half_box", (data) => {
- if (data.face == "top") {
-  if (data.neighborVoxel.getShapeState() == 0) {
-   return true;
+  if (data.face == "top") {
+    if (data.neighborVoxel.getShapeState() == 0) {
+      return true;
+    }
+    return false;
   }
-  return false;
- }
- return true;
+  return true;
 });
 
 const StairCullFunctions: Record<
- DirectionNames,
- (data: FaceDataOverride) => boolean
+  DirectionNames,
+  (data: FaceDataOverride) => boolean
 > = {
- top: (data) => {
-  const nVoxelShapeState = data.neighborVoxel.getShapeState();
-  if (
-   (nVoxelShapeState >= 0 && nVoxelShapeState <= 3) ||
-   (nVoxelShapeState >= 8 && nVoxelShapeState <= 11)
-  ) {
-   return false;
-  }
-  return true;
- },
- bottom: (data) => {
-  const nVoxelShapeState = data.neighborVoxel.getShapeState();
-  if (
-   (nVoxelShapeState >= 4 && nVoxelShapeState <= 7) ||
-   (nVoxelShapeState >= 12 && nVoxelShapeState <= 15)
-  ) {
-   return false;
-  }
-  return true;
- },
- east: (data) => {
-  const nVoxelShapeState = data.neighborVoxel.getShapeState();
-  if (nVoxelShapeState == 1 || nVoxelShapeState == 5) return false;
-  return true;
- },
- west: (data) => {
-  const nVoxelShapeState = data.neighborVoxel.getShapeState();
-  if (nVoxelShapeState == 3 || nVoxelShapeState == 7) return false;
-  return true;
- },
- north: (data) => {
-  const nVoxelShapeState = data.neighborVoxel.getShapeState();
-  if (nVoxelShapeState == 0 || nVoxelShapeState == 4) return false;
-  return true;
- },
- south: (data) => {
-  const nVoxelShapeState = data.neighborVoxel.getShapeState();
-  if (nVoxelShapeState == 2 || nVoxelShapeState == 6) return false;
-  return true;
- },
+  top: (data) => {
+    const nVoxelShapeState = data.neighborVoxel.getShapeState();
+    if (
+      (nVoxelShapeState >= 0 && nVoxelShapeState <= 3) ||
+      (nVoxelShapeState >= 8 && nVoxelShapeState <= 11)
+    ) {
+      return false;
+    }
+    return true;
+  },
+  bottom: (data) => {
+    const nVoxelShapeState = data.neighborVoxel.getShapeState();
+    if (
+      (nVoxelShapeState >= 4 && nVoxelShapeState <= 7) ||
+      (nVoxelShapeState >= 12 && nVoxelShapeState <= 15)
+    ) {
+      return false;
+    }
+    return true;
+  },
+  east: (data) => {
+    const nVoxelShapeState = data.neighborVoxel.getShapeState();
+    if (nVoxelShapeState == 1 || nVoxelShapeState == 5) return false;
+    return true;
+  },
+  west: (data) => {
+    const nVoxelShapeState = data.neighborVoxel.getShapeState();
+    if (nVoxelShapeState == 3 || nVoxelShapeState == 7) return false;
+    return true;
+  },
+  north: (data) => {
+    const nVoxelShapeState = data.neighborVoxel.getShapeState();
+    if (nVoxelShapeState == 0 || nVoxelShapeState == 4) return false;
+    return true;
+  },
+  south: (data) => {
+    const nVoxelShapeState = data.neighborVoxel.getShapeState();
+    if (nVoxelShapeState == 2 || nVoxelShapeState == 6) return false;
+    return true;
+  },
 };
 
 //cull leaf faces
 const BoxCullFunctions: Record<
- DirectionNames,
- (data: FaceDataOverride) => boolean
+  DirectionNames,
+  (data: FaceDataOverride) => boolean
 > = {
- top: (data) => {
-  if (
-   data.currentVoxel.getSubstance() == "#dve_flora" &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2] + 1,
-    data.currentVoxel.location[3]
-   ) &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2] + 2,
-    data.currentVoxel.location[3]
-   )
-  ) {
-   return false;
-  }
-  return data.default;
- },
- bottom: (data) => {
-  if (
-   data.currentVoxel.getSubstance() == "#dve_flora" &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2] - 1,
-    data.currentVoxel.location[3]
-   ) &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2] - 2,
-    data.currentVoxel.location[3]
-   )
-  ) {
-   return false;
-  }
-  return data.default;
- },
- east: (data) => {
-  if (
-   data.currentVoxel.getSubstance() == "#dve_flora" &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1] + 1,
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3]
-   ) &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1] + 2,
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3]
-   )
-  ) {
-   return false;
-  }
-  return data.default;
- },
- west: (data) => {
-  if (
-   data.currentVoxel.getSubstance() == "#dve_flora" &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1] - 1,
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3]
-   ) &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1] - 2,
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3]
-   )
-  ) {
-   return false;
-  }
-  return data.default;
- },
- north: (data) => {
-  if (
-   data.currentVoxel.getSubstance() == "#dve_flora" &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3] + 1
-   ) &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3] + 2
-   )
-  ) {
-   return false;
-  }
-  return data.default;
- },
- south: (data) => {
-  if (
-   data.currentVoxel.getSubstance() == "#dve_flora" &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3] - 1
-   ) &&
-   data.currentVoxel.isSameVoxel(
-    data.currentVoxel.location[1],
-    data.currentVoxel.location[2],
-    data.currentVoxel.location[3] - 2
-   )
-  ) {
-   return false;
-  }
-  return data.default;
- },
+  top: (data) => {
+    if (
+      data.currentVoxel.getSubstance() == "#dve_flora" &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2] + 1,
+        data.currentVoxel.location[3]
+      ) &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2] + 2,
+        data.currentVoxel.location[3]
+      )
+    ) {
+      return false;
+    }
+    return data.default;
+  },
+  bottom: (data) => {
+    if (
+      data.currentVoxel.getSubstance() == "#dve_flora" &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2] - 1,
+        data.currentVoxel.location[3]
+      ) &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2] - 2,
+        data.currentVoxel.location[3]
+      )
+    ) {
+      return false;
+    }
+    return data.default;
+  },
+  east: (data) => {
+    if (
+      data.currentVoxel.getSubstance() == "#dve_flora" &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1] + 1,
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3]
+      ) &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1] + 2,
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3]
+      )
+    ) {
+      return false;
+    }
+    return data.default;
+  },
+  west: (data) => {
+    if (
+      data.currentVoxel.getSubstance() == "#dve_flora" &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1] - 1,
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3]
+      ) &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1] - 2,
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3]
+      )
+    ) {
+      return false;
+    }
+    return data.default;
+  },
+  north: (data) => {
+    if (
+      data.currentVoxel.getSubstance() == "#dve_flora" &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3] + 1
+      ) &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3] + 2
+      )
+    ) {
+      return false;
+    }
+    return data.default;
+  },
+  south: (data) => {
+    if (
+      data.currentVoxel.getSubstance() == "#dve_flora" &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3] - 1
+      ) &&
+      data.currentVoxel.isSameVoxel(
+        data.currentVoxel.location[1],
+        data.currentVoxel.location[2],
+        data.currentVoxel.location[3] - 2
+      )
+    ) {
+      return false;
+    }
+    return data.default;
+  },
 };
