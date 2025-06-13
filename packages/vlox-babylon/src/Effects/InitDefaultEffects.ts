279e41feab529d50c5be176eacf4f6a75bece605
Lucas Damian Johnson <60717791+lucasdamianjohnson@users.noreply.github.com>
2025-06-13 19:04:54 -0400

    removed vlox babylon

--- packages/vlox-babylon/src/Effects/InitDefaultEffects.ts
+++
@@ -1,6 +0,0 @@
-import { VoxelEffectRegister } from "@divinevoxel/vlox/Voxels/Effects/VoxelEffectRegister";
-import { CandleParticles } from "./Default/CandleParticles";
-
-export default function () {
-  VoxelEffectRegister.register(CandleParticles);
-}
